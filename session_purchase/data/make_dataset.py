c3a6c1ded9662ef0c7ccef49ac41520bdc1c6183
bartooo <bcywinski11@gmail.com>
2022-01-09 00:10:57 +0100

    Final model selection

--- session_purchase/data/make_dataset.py
+++ session_purchase/data/make_dataset.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
 from typing import Tuple, Any
-
-import click
 import logging
-from pathlib import Path
-
 import pandas as pd
 from pandas import DataFrame
 from sklearn.model_selection import train_test_split
@@ -57,16 +52,6 @@ def encode_dates(main_df):
     })
 
 
-def dummy_sum(a, b):
-    """Used exclusively to showcase relative imports in tests. See
-       tests/test_make_dataset.py in the repo.
-    """
-    return a + b
-
-
-# @click.command()
-# @click.argument('input_filepath', type=click.Path(exists=True))
-# @click.argument('output_filepath', type=click.Path())
 def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -117,10 +102,4 @@ def main():
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    # logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
     main()
